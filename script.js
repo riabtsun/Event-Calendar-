a5f602adb54b5a508b83ede4d6acc7adaf02e797
Dmytro Riabtsun <riabtsun@i.ua>
2021-04-15 21:42:14 +0300

    add delete btn, and local storage

--- script.js
+++ script.js
@@ -32,10 +32,20 @@ const mettingHours = {
 }
 
 class Calendar {
-	constructor(calendar, tableId) {
-		this.calendar = calendar;
+	constructor(tableId) {
 		this.tableId = tableId;
 	}
+
+	set data(data) {
+		if (!localStorage.calendar) {
+			localStorage.setItem('calendar', JSON.stringify(data));
+		}
+	}
+
+	get data() {
+		return JSON.parse(localStorage.calendar)
+	}
+
 	renderCalendar() {
 		let thead = days.map(day => `<th>${day}</th>`)
 
@@ -63,42 +73,62 @@ class Calendar {
 	}
 
 	infoCalendar() {
-		let calendarInfo = this.calendar;
+		let calendarInfo = this.data;
 		for (let key in calendarInfo) {
 			let day = calendarInfo[key];
 			for (let hour in day) {
 				let event = day[hour]
 				let table = document.querySelector(`#${this.tableId}`),
 					currentTd = table.querySelector(`td[data-id=${key}-${hour}]`);
-				currentTd.innerHTML = this.renderEvent(event);
+				currentTd.append(this.renderEvent(event));
 			}
 		}
 	}
-	renderEvent(event) {
-		let deleteBtn = document.createElement('button');
-		deleteBtn.classList.add('event__delete');
-		deleteBtn.innerHTML = 'X';
-		deleteBtn.addEventListener('click', this.cancelEvent);
 
+	renderEvent(event) {
 		let eventDiv = document.createElement('div');
 		eventDiv.classList.add('event');
-		eventDiv.append(deleteBtn)
 
-		let eventRender = `<div class='event'>
-			<p class='event__name'>${event.name}</p>
-			<p class='event__users'>${event.users.join(', ')}</p>
-		</div>`;
-		return eventRender;
+		let deleteBtn = document.createElement('button');
+		deleteBtn.classList.add('event__delete');
+		deleteBtn.innerHTML = 'x';
+		deleteBtn.addEventListener('click', this.cancelEvent);
+
+		let name = document.createElement('p');
+		name.classList.add(`event__name`);
+		name.innerHTML = `<b>${event.name}</b>`;
+
+		let users = document.createElement('p');
+		users.classList.add(`event__users`);
+		users.innerHTML = event.users.join(', ');
+
+		eventDiv.append(name, users, deleteBtn);
+
+		return eventDiv;
 	}
-	cancelEvent() {
 
+	cancelEvent() {
+		let cancelBtn = this;
+		let currentTD = cancelBtn.closest('td'),
+			currentTDid = currentTD.dataset.id;
+
+		let data = currentTDid.split('-');
+		let day = data[0],
+			hour = data[1];
+
+		let calendar = JSON.parse(localStorage.calendar);
+		delete calendar[day][hour];
+		localStorage.calendar = JSON.stringify(calendar);
+
+		currentTD.innerHTML = '';
 	}
 }
 
-let mettingCalendar = new Calendar(data, 'roomGreen');
+let mettingCalendar = new Calendar('roomGreen');
+mettingCalendar.data = data;
 console.log(mettingCalendar);
 
 mettingCalendar.renderCalendar();
 mettingCalendar.infoCalendar();
 
-// time 1:00:00
+// time 1:48:00
