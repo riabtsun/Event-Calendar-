8c901ef8c263dc9701f775f29c8bcd9d8eaa8fbc
Dmytro Riabtsun <riabtsun@i.ua>
2021-04-18 16:51:28 +0300

    final

---
+++ form.js
@@ -0,0 +1,46 @@
+let form = document.querySelector('#addEvent');
+
+let calendarName = document.location.search;
+calendarName = calendarName.replace('?', '');
+calendarNameArr = calendarName.split('=')
+
+calendarName = calendarNameArr[1];
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  let form = this;
+
+  let inputName = form.querySelector('#name'),
+    inputNameValue = inputName.value;
+
+  let selectedUsers = form.querySelector('#users'),
+    selectUsersValue = [...selectedUsers.selectedOptions];
+
+  selectedUsersValue = selectUsersValue.map(function (option) {
+    return option.value
+  })
+
+  let selectedDay = form.querySelector('#day'),
+    selectedDayValue = selectedDay.value;
+
+  let selectedHour = form.querySelector('#hours'),
+    selectedHourValue = selectedHour.value;
+
+  let data = JSON.parse(localStorage[calendarName]);
+
+  if (data[selectedDayValue] && data[selectedDayValue][selectedHourValue]) {
+    alert('Event for this day and hour already exist. Please, change day or hour.')
+  } else {
+    if (!data[selectedDayValue]) {
+      data[selectedDayValue] = {};
+    }
+    data[selectedDayValue][selectedHourValue] = {
+      name: inputNameValue,
+      users: selectedUsersValue
+
+    }
+
+    localStorage[calendarName] = JSON.stringify(data)
+  }
+})
--- script.js
+++ script.js
@@ -105,27 +105,49 @@ class Calendar {
 		</table>`;
 
 		let meetingRoom = document.querySelector('#meetingRoom');
+
+		let usersSelect = this.renderUsers();
+
+		let addEventBtn = `<a class='btn' href="addEvent.html?calendar=${this.tableId}" target="_blank">Add event</a>`;
+
+		let calendar__options = document.createElement('div');
+		calendar__options.classList.add('calendar__options');
+		calendar__options.append(usersSelect);
+		calendar__options.innerHTML += addEventBtn;
+
+		meetingRoom.append(calendar__options);
 		meetingRoom.innerHTML += table;
 	}
 
-	infoCalendar() {
-		let calendarInfo = this.data;
-		this.renderUsers()
+	infoCalendar(data) {
+		let calendarInfo = data ? data : this.data;
+		let table = document.querySelector(`#${this.tableId}`),
+			tds = table.querySelectorAll('td:nth-child(n+2)');
+
+		Calendar.emptyTDs(tds)
 
 		for (let key in calendarInfo) {
 			let day = calendarInfo[key];
 
 			for (let hour in day) {
-				let event = day[hour];
-				let table = document.querySelector(`#${this.tableId}`),
+				let event = day[hour],
 					currentTD = table.querySelector(`td[data-id=${key}-${hour}]`);
 
-				currentTD.append(this.renderEvent(event));
+				if (event !== null) {
+					currentTD.append(this.renderEvent(event));
+				}
+
 			}
 		}
 	}
 
-	renderUsers() {
+	static emptyTDs(tds) {
+		tds.forEach(function (td) {
+			td.innerHTML = ``
+		})
+	}
+
+	renderUsers(parentElement) {
 		let calendarInfo = this.data;
 
 		let users = [];
@@ -136,12 +158,43 @@ class Calendar {
 				users.push(day[hour].users)
 			}
 		}
-		// 29:55
+
 		let all_users = users.flat().filter(
 			function (value, index, self) {
 				return self.indexOf(value) === index;
 			}
-		)
+		).map(function (user) {
+			return `<option value='${user}'>${user}</option>`
+		});
+		all_users.unshift('<option vlue="all">All users</option>')
+
+		let select = document.createElement('select');
+		select.dataset.name = "usersSelect";
+		select.dataset.id = this.tableId;
+		select.innerHTML = all_users.join('');
+
+		return select
+	}
+
+	static userEvents() {
+		let userSelected = this.value,
+			tableId = this.dataset.id;
+
+		let data = JSON.parse(localStorage[tableId]);
+
+		if (userSelected !== 'all') {
+			for (let key in data) {
+				let day = data[key];
+				for (let hour in day) {
+					let hourEvent = day[hour];
+					if (!hourEvent.users.includes(userSelected)) {
+						day[hour] = null;
+					}
+				}
+			}
+		}
+
+		roomGreen.infoCalendar(data);
 	}
 
 	renderEvent(event) {
@@ -185,16 +238,16 @@ class Calendar {
 	}
 }
 
-let roomGreen = new Calendar('roomGreen', ['monday', 'tuesday', 'friday'], { start: 10, end: 18 }, '#55da72 ');
+let roomGreen = new Calendar('roomGreen', ['monday', 'tuesday', 'wednesday', 'thirsday', 'friday', 'saturday', 'sunday'], { start: 10, end: 18 }, '#55da72 ');
 roomGreen.data = dataGreenRoom;
 roomGreen.renderCalendar();
 roomGreen.infoCalendar();
 
 
-let roomRed = new Calendar('roomRed', ['wednesday', 'thursday',], { start: 9, end: 20 }, 'red');
-roomRed.data = dataRedRoom;
-roomRed.renderCalendar();
-roomRed.infoCalendar();
+// let roomRed = new Calendar('roomRed', ['wednesday', 'thursday',], { start: 9, end: 20 }, 'red');
+// roomRed.data = dataRedRoom;
+// roomRed.renderCalendar();
+// roomRed.infoCalendar();
 
 let cancelBTNs = document.querySelectorAll('.event__cancel');
 
@@ -202,4 +255,9 @@ cancelBTNs.forEach(function (btn) {
 	btn.addEventListener('click', function () {
 		Calendar.cancelEvent(btn);
 	})
+});
+
+let usersSelects = document.querySelectorAll('select[data-name="usersSelect"]');
+usersSelects.forEach(function (select) {
+	select.addEventListener('change', Calendar.userEvents)
 })
